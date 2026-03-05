57fa2889507b071186849dc5323e0729fad02be2
Varun Kumar <varunkumar17135176@gmail.com>
2026-03-05 22:29:51 +0530

    jwt

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,7 +3,8 @@ const router=express.Router();
 const mongoose=require('mongoose');
 const User=mongoose.model("User");
 const bcrypt=require('bcrypt');
-
+const jwt=require('jsonwebtoken');
+const {JWT_SECRET}=require('./key');
 router.get('/',(req,res)=>{
   res.send("hello")
 })
@@ -11,7 +12,7 @@ router.post('/signup',(req,res)=>{
   const {name,email,password}=req.body;
   if(!email || !password || !name)
   {
-    res.status(422).json({error:"please add all the fields"});
+    return res.status(422).json({error:"please add all the fields"});
   }
   User.findOne({email:email}).then((saveduser)=>{
     if(saveduser){
@@ -31,6 +32,36 @@ router.post('/signup',(req,res)=>{
       .catch(error=>{
         console.log(error);
       })
+    })
+    .catch(error=>{
+      console.log(error);
+    })
+  })
+})
+
+router.post('/signin',(req,res)=>{
+  const {email,name,password}=req.body;
+  if(!email || !password)
+  {
+    return res.status(422).json({error:"please provide email and password"});
+  }
+  User.findOne({email:email})
+  .then(saveduser=>{
+    if(!saveduser)
+    {
+      return res.status(422).json({error:"invalid email or invalid password"});
+    }
+    bcrypt.compare(password,saveduser.password)
+    .then(domatch=>{
+      if(domatch)
+      {
+        const token = jwt.sign({_id:saveduser._id}, JWT_SECRET, {expiresIn:'7d'});
+        res.json({token});
+      }
+      else
+      {
+        return res.status(422).json({error:"invalid email or invalid password"});
+      }
     })
     .catch(error=>{
       console.log(error);
