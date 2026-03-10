2c32beddc4ba99029146629214530b52609a8a1d
Varun Kumar <varunkumar17135176@gmail.com>
2026-03-10 23:31:15 +0530

    signup and login page

--- frontend/client/src/component/screens/Login.js
+++ frontend/client/src/component/screens/Login.js
@@ -1,7 +1,18 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 const Login=()=>{
   return(
-    <h1>Login</h1>
+    <div className="mycard">
+      <div className="card auth-card">
+        <h2 className="brand-logo">Social</h2>
+        <input type="text" placeholder="email"></input>
+        <input type="text" placeholder="password"></input>
+        <a className="waves-effect waves-light btn">Login In</a>
+        <h5>
+          <Link to='/signup'>do not have an account</Link>
+        </h5>
+      </div>
+  </div>
   )
 }
 export default Login;
--- frontend/client/src/component/screens/Signup.js
+++ frontend/client/src/component/screens/Signup.js
@@ -1,7 +1,19 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 const Signup=()=>{
   return(
-    <h1>Signup</h1>
-  )
+    <div className="mycard">
+      <div className="card auth-card">
+        <h2 className="brand-logo">Social</h2>
+         <input type="text" placeholder="name"></input>
+        <input type="text" placeholder="email"></input>
+        <input type="text" placeholder="password"></input>
+        <a className="waves-effect waves-light btn">Login In</a>
+        <h5>
+          <Link to='/login'>already have an account</Link>
+        </h5>
+      </div>
+  </div>
+  );
 }
 export default Signup;
