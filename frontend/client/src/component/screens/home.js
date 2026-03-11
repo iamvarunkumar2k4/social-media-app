514169b5f6bd688192139e3d6f638298908c1d15
Varun Kumar <varunkumar17135176@gmail.com>
2026-03-11 22:28:02 +0530

    home page

--- frontend/client/src/component/screens/home.js
+++ frontend/client/src/component/screens/home.js
@@ -1,7 +1,56 @@
 import React from "react";
 const Home=()=>{
   return(
-    <h1>Home</h1>
+    <div className="home">
+      <div className="card home-card">
+        <h5>Varun</h5>
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1499678329028-101435549a4e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZGVza3RvcCUyMHdhbGxwYXBlcnxlbnwwfHwwfHx8MA%3D%3D"></img>
+        </div>
+        <div className="card-content">
+          <i className="material-icons" style={{color:"pink"}}>favorite</i>
+          <h6>title</h6>
+          <p>this is amazing</p>
+          <input type="text" placeholder="enter commenmt"></input>
+        </div>
+      </div>
+       <div className="card home-card">
+        <h5>Varun</h5>
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1499678329028-101435549a4e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZGVza3RvcCUyMHdhbGxwYXBlcnxlbnwwfHwwfHx8MA%3D%3D"></img>
+        </div>
+        <div className="card-content">
+          <i className="material-icons" style={{color:"pink"}}>favorite</i>
+          <h6>title</h6>
+          <p>this is amazing</p>
+          <input type="text" placeholder="enter commenmt"></input>
+        </div>
+      </div>
+       <div className="card home-card">
+        <h5>Varun</h5>
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1499678329028-101435549a4e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZGVza3RvcCUyMHdhbGxwYXBlcnxlbnwwfHwwfHx8MA%3D%3D"></img>
+        </div>
+        <div className="card-content">
+          <i className="material-icons" style={{color:"pink"}}>favorite</i>
+          <h6>title</h6>
+          <p>this is amazing</p>
+          <input type="text" placeholder="enter commenmt"></input>
+        </div>
+      </div>
+       <div className="card home-card">
+        <h5>Varun</h5>
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1499678329028-101435549a4e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZGVza3RvcCUyMHdhbGxwYXBlcnxlbnwwfHwwfHx8MA%3D%3D"></img>
+        </div>
+        <div className="card-content">
+            <i className="material-icons" style={{color:"pink"}}>favorite</i>
+          <h6>title</h6>
+          <p>this is amazing</p>
+          <input type="text" placeholder="enter commenmt"></input>
+        </div>
+      </div>
+    </div>
   )
 }
 export default Home;
